7ceb8af2181fb6eff0c74adab02d191acb1213cf
tranthanh2312 <tranquangthanh9615@gmail.com>
2023-03-15 21:12:29 +0700

    first commit

---
+++ __MACOSX/hbhbhb.js
@@ -0,0 +1,6 @@
+// const data = [{firstName:"enennfejnf"},{firstName:"jfewf"},{firstName:"fef"}];
+// for(let i=0; i>data.firstName; i++){
+//     console.log('array[i]');
+// }
+var i=10;
+console.log(i);
